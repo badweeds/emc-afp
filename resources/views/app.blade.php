c3b06a3e667a795967562306f0da02da29e9b3fc
badweeds <tom.rapliza@hcdc.edu.ph>
2026-05-25 11:27:57 +0800

    update for today

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,17 +3,39 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Favicons -->
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon/favicon.ico') }}">
+
+        <link rel="icon" type="image/png" sizes="16x16"
+              href="{{ asset('images/favicon/favicon-16x16.png') }}">
+
+        <link rel="icon" type="image/png" sizes="32x32"
+              href="{{ asset('images/favicon/favicon-32x32.png') }}">
+
+        <link rel="apple-touch-icon" sizes="180x180"
+              href="{{ asset('images/favicon/apple-touch-icon.png') }}">
+
+        <link rel="icon" type="image/png" sizes="192x192"
+              href="{{ asset('images/favicon/android-chrome-192x192.png') }}">
+
+        <link rel="icon" type="image/png" sizes="512x512"
+              href="{{ asset('images/favicon/android-chrome-512x512.png') }}">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+              rel="stylesheet">
 
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
+
     <body class="font-sans antialiased">
         @inertia
     </body>
